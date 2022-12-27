8de813d1f85d8827a48b6d57306fc9c78e69ac49
Shady Amr <shadyamr@outlook.com>
2022-12-27 04:32:02 +0200

    Removed old data

--- old-mvc-backup/config/database.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
--- old-mvc-backup/views/html/header.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo SITE_NAME; ?></title>
-    <link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
-    <script src="public/assets/js/bootstrap.bundle.min.js"></script>
-</head>
