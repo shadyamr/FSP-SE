062884980a282c98f005a94ec2b43709e8679046
Shady Amr <contact@shadyamr.ml>
2023-01-02 15:31:53 +0200

    Updated the dashboard with stats

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\RequestsForm;
+
+use App\Models\Inspections;
+
+use App\Models\User;
+
+use App\Models\Category;
+
+use App\Models\Item;
+
+use App\Models\Supplier;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +35,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $requests = RequestsForm::count();
+        $inspections = Inspections::count();
+        
+        $categories = Category::count();
+        $items = Item::count();
+        $suppliers = Supplier::count();
+
+        $employees = User::count();
+        return view('home', compact('requests', 'inspections', 'categories', 'items', 'suppliers', 'employees'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,72 @@
 <div class="container">
     <div class="row justify-content-center">
         @if (session('access'))
-            <div class="alert alert-danger" role="alert">
-                {{ session('access') }}
-            </div>
+        <div class="alert alert-danger" role="alert">
+            {{ session('access') }}
+        </div>
         @endif
-        <div class="col-md-8">
+        @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+        @endif
+        <div class="col-sm-4 mt-2">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="card-header text-center">
+                    Requests
+                </div>
+                <div class="card-body text-center">
+                    <p class="card-text"><span class="badge bg-dark">{{ $requests }}</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-2">
+            <div class="card">
+                <div class="card-header text-center">
+                    Inspections
+                </div>
+                <div class="card-body text-center">
+                    <p class="card-text"><span class="badge bg-dark">{{ $inspections }}</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-2">
+            <div class="card">
+                <div class="card-header text-center">
+                    Categories
+                </div>
+                <div class="card-body text-center">
+                    <p class="card-text"><span class="badge bg-dark">{{ $categories }}</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-2">
+            <div class="card">
+                <div class="card-header text-center">
+                    Items
+                </div>
+                <div class="card-body text-center">
+                    <p class="card-text"><span class="badge bg-dark">{{ $items }}</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-2">
+            <div class="card">
+                <div class="card-header text-center">
+                    Suppliers
+                </div>
+                <div class="card-body text-center">
+                    <p class="card-text"><span class="badge bg-dark">{{ $suppliers }}</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-2">
+            <div class="card">
+                <div class="card-header text-center">
+                    Employees
+                </div>
+                <div class="card-body text-center">
+                    <p class="card-text"><span class="badge bg-dark">{{ $employees }}</span></p>
                 </div>
             </div>
         </div>
